611f8af6e19e792e1d08c014884af12b333852dd
purkylin <purkylin@gmail.com>
2019-10-09 14:06:41 +0800

    Initial Commit

---
+++ NFC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NFC
+//
+//  Created by Purkylin King on 2019/10/9.
+//  Copyright © 2019 Purkylin King. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
